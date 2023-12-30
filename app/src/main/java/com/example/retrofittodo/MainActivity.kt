7c883984d4fac2f5083bbf6db6aeac82c2f34094
Siddharth Manthan <siddharth.manthan@gmail.com>
2023-12-30 21:42:13 +0530

    v2: Add user selection

--- app/src/main/java/com/example/retrofittodo/MainActivity.kt
+++ app/src/main/java/com/example/retrofittodo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleCoroutineScope
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
